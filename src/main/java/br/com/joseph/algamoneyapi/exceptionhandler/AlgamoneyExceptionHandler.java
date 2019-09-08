2014a188af55998e8445bd5bb0bbd77f5ab0467f
Joseph <joseluis.bsa@gmail.com>
2019-09-08 20:32:45 -0300

    melhorado dev msg de retorno

--- src/main/java/br/com/joseph/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/br/com/joseph/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -54,6 +56,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+		List<Error> erros = Arrays.asList(new Error(userMessage, devMessage));
+
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+	
+	
 
 	private List<Error> createListOfErrors(BindingResult bindingResult) {
 		List<Error> errors = new ArrayList<>();
