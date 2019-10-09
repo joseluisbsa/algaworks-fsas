7b30c42f4a5c0073d1fd2300dce8d6c21208f46b
Joseph <joseluis.bsa@gmail.com>
2019-10-08 21:10:28 -0300

    Alternando OAuth 2 e Basic Security com profiles

--- src/main/java/br/com/joseph/algamoneyapi/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/joseph/algamoneyapi/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.joseph.algamoneyapi.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
